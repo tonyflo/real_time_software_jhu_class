abb19b30903360f81f6a24cb184a217adf5155ab
Tony Florida <tflorid1@jhu.edu>
2014-10-07 21:20:34 -0400

    Closed to finished HW3

--- HW3/florida_hw3/florida_hw3.ino
+++ HW3/florida_hw3/florida_hw3.ino
@@ -9,9 +9,9 @@
 // http://techvalleyprojects.blogspot.com/2012/06/arduino-control-escmotor-tutorial.html
 // https://github.com/Zuph/AVRQueue
 
-//CMD variables (range of 20 to 160)
-int serial_cmd = 10; //serial commands start at 10
-int MAX_SERIAL_CMD = 160; //max serial command
+//CMD variables (range of 15 to 150)
+int serial_cmd = 5; //serial commands start at 10
+int MAX_SERIAL_CMD = 100; //max serial command
 int SERIAL_CMD_INCREMENT = 5; //increment serial commands by 20
 
 //Queue variables
@@ -50,12 +50,10 @@ void rpm_fun()
    digitalWrite(statusPin, status);
 }
 
-int cmd(unsigned long now)
+int cmd()
 {
-  Serial.write(serial_cmd);
-  Serial.write(10);
   serial_cmd+=SERIAL_CMD_INCREMENT; //increment the serial command
-  if(serial_cmd >= MAX_SERIAL_CMD)
+  if(serial_cmd > MAX_SERIAL_CMD)
   {
     SERIAL_CMD_INCREMENT *= -1; //incrementally spin down the motors
   }
@@ -63,11 +61,10 @@ int cmd(unsigned long now)
   if(serial_cmd <= 0)
   {
       // we are done!
-      myQueue.scheduleRemoveFunction("CMD");
       myQueue.scheduleRemoveFunction("ESC");
   }
   
-  return 1;
+  return serial_cmd;
 }
 
 void setup() {
@@ -96,9 +93,15 @@ void setup() {
 
     //Function queue scheduling setup
     Serial.println("Initializing function queue scheduling");
-    myQueue.scheduleFunction(esc, "ESC", 5000, 1000);
-    myQueue.scheduleFunction(cmd, "CMD", 7000, 4000);
+    myQueue.scheduleFunction(esc, "ESC", 0, 3000);
 
+   //Print table header
+   Serial.println("Time(ms),RPM,Command");
+   
+   //Wait until start command i.e. any input serial comms
+   Serial.println("Plug battery into motors, then send any serial command");
+   while(!Serial.available()) {}
+   
     while(1) {
         myQueue.Run(millis());
         rpm_counter();
@@ -110,73 +113,19 @@ void setup() {
 //Receive ESC commands via serial
 int esc(unsigned long now)
 {
-  // If there is incoming value
-  if(Serial.available() > 0)
-  {
-    // read the value
-    char ch = Serial.read();
-  
-    /*
-    *  If ch isn't a newline
-    *  (linefeed) character,
-    *  we will add the character
-    *  to the incomingString
+   int val = cmd(); //new rotation speed
+
+   /*  We only want to write an integer between
+    *  0 and 180 to the motor. 
     */
-    if (ch != 10){
-      // Print out the value received
-      // so that we can see what is
-      // happening
-      Serial.print("I have received: ");
-      Serial.print(ch, DEC);
-      Serial.print('\n');
-    
-      // Add the character to
-      // the incomingString
-      incomingString += ch;
-    }
-    // received a newline (linefeed) character
-    // this means we are done making a string
-    else
-    {
-      // print the incoming string
-      Serial.println("I am printing the entire string");
-      Serial.println(incomingString);
-    
-      // Convert the string to an integer
-      int val = incomingString.toInt();
-    
-      // print the integer
-      Serial.println("Printing the value: ");
-      Serial.println(val);
-    
-      /*
-      *  We only want to write an integer between
-      *  0 and 180 to the motor. 
-      */
-      if (val > -1 && val < 181)
-      {
+   if (val > -1 && val < 181)
+   {
        // Print confirmation that the
        // value is between 0 and 180
-       Serial.println("Value is between 0 and 180");
        // Write to Servo
        myMotor.write(val);
        myMotor2.write(val);
-      }
-      // The value is not between 0 and 180.
-      // We do not want write this value to
-      // the motor.
-      else
-      {
-       Serial.println("Value is NOT between 0 and 180");
-      
-       // IT'S a TRAP!
-       Serial.println("Error with the input");
-      } //0 to 180
-    
-      // Reset the value of the incomingString
-      incomingString = "";
-    } //ch not 10
-  }
+   }
 }
 
 //Count RPMs
@@ -195,8 +144,10 @@ void rpm_counter()
    
    //Write it out to serial port
    Serial.print(millis());
-   Serial.print(" ");
-   Serial.println(rpm,DEC);
+   Serial.print(",");
+   Serial.print(rpm,DEC);
+   Serial.print(",");
+   Serial.println(serial_cmd);
    
    //Restart the interrupt processing
    attachInterrupt(0, rpm_fun, FALLING);

070fd59cccdccab140921befc8d12994a2200ba4
Tony Florida <tflorid1@jhu.edu>
2014-11-24 19:40:52 -0500

    Converted app to parse serial commands and write to motors

--- Quadcopter/bbb2arduino/bbb2arduino.ino
+++ Quadcopter/bbb2arduino/bbb2arduino.ino
@@ -1,15 +1,102 @@
 // Recieve roll, pitch, yaw from BBB via USB serial
-// http://electronics.stackexchange.com/questions/45543/how-do-i-receive-an-entire-string-as-opposed-to-1-character-at-a-time-on-the-ard
 
-int led = 3;
-int state = 1;
-String rpy;
+#include <Servo.h>
 
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
+//constants
+#define num_cmds 4 //4 commands, 1 for each motor
+
+//variables
+String command; // accumulating string read from serail
+String command_arr[num_cmds]; //array of strings
+int command_val[num_cmds]; //array of ints
+int command_i; // counter to keep track of which value we're parsing
+
+//objects
+Servo motor06;
+Servo motor09;
+Servo motor10;
+Servo motor11;
+
+void variableReset()
+{ 
+  for(int i = 0; i < num_cmds; i++)
+  {
+    command_arr[i] = "";
+  }
+  command_i = 0;
+  command = ""; 
+}
+
+void setup() {       
+  // Serial init  
   Serial.begin(9600);
-  digitalWrite(led, state);
+  
+  // Variable init
+  variableReset();
+  
+  // Associate the motors at the appropriate Arduino pins
+  motor06.attach(6);
+  motor09.attach(9);
+  motor10.attach(10);
+  motor11.attach(11);
+}
+
+// When a delimeter has been found, save the value (r, p, y) into an array
+void save_value()
+{
+  // Save the current r or p or y to the array
+  command_arr[command_i]=command;
+  
+  // Moving onto next index in sequence of r, p, y
+  command="";
+  command_i++; 
+}
+
+void update_command()
+{
+  boolean bad = false;
+  
+  // Check for an invalid command
+  for(int i = 0; i < num_cmds; i++)
+  {
+    if( command_arr[i] == "")
+    {
+      Serial.print("bad @ ");
+      Serial.println(i);
+      // This command seems to be invalid, throw it away
+      bad = true;
+      break; 
+    }
+    else
+    {
+      Serial.print("good ");
+      Serial.println( command_arr[i]);
+    }
+  }
+  
+  if(!bad)
+  {
+    // We have a valid commmand so update the motor values
+    for(int i = 0; i < num_cmds; i++)
+    {
+      command_val[i]=command_arr[i].toInt();
+    }
+  }
+}
+
+void spin_motors()
+{
+  // Spin the motors
+  for(int i = 0; i < num_cmds; i++)
+  {
+    Serial.print(command_val[i]);
+    Serial.print('\t');
+  }
+  Serial.print('\n');  
+  motor06.writeMicroseconds(command_val[0]);
+  motor09.writeMicroseconds(command_val[1]);
+  motor10.writeMicroseconds(command_val[2]);
+  motor11.writeMicroseconds(command_val[3]);
 }
 
 void loop() {
@@ -17,22 +104,28 @@ void loop() {
   {
     // Read serial character
     char inChar = Serial.read();
-    
-    // Accumulate the roll, pitch, yaw string one character at a time
-    rpy += inChar;
      
-    // New line signals end of roll, pitch, yaw string
-    if(inChar == '\n')
+    if(inChar == ',')
     {
-      //Test BBB comms
-      if(rpy == "1,2,3\n")
-      {
-        state = state ? 0 : 1;
-        digitalWrite(led, state);
-      }
+      save_value();
+    }
+    // New line signals end command string
+    else if(inChar == '\n')
+    {
+      save_value();
       
-      // Clear roll, pitch, yaw string
-      rpy="";
+      //Update with a new command
+      update_command();
+      
+      // Ready for next value
+      variableReset();
+    }
+    else
+    {
+      // Accumulate the command string one character at a time
+      command += inChar;
     }
   }
+  
+  spin_motors();
 }
